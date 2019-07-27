cb86af37044c8e1284daccdc42761680a71ae47b
Kos <kostek001@gmail.com>
2019-07-27 20:17:15 +0300

    Add connection string to the Api, fix entity

--- CarAds/CarAds.Data/Configurations/ConditionTypeConfiguration.cs
+++ CarAds/CarAds.Data/Configurations/ConditionTypeConfiguration.cs
@@ -8,7 +8,7 @@ namespace CarAds.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<ConditionTypeEntity> builder)
         {
-            builder.ToTable("FuelType");
+            builder.ToTable("ConditionType");
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
         }
